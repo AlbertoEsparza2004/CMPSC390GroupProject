3e32da8f9d2d94e2b012cbda325c7a87023bd93d
Alberto <esparza009@sxu.edu>
2026-02-28 20:58:09 -0600

    After the big pull

---
+++ backend/db.js
@@ -0,0 +1,18 @@
+const mysql = require("mysql2");
+
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",//put your MySQL root password here if you set one
+  database: "legautocustDB"
+});
+
+db.connect((err) => {
+  if (err) {
+    console.log("DB connection failed:", err);
+    return;
+  }
+  console.log("Connected to MySQL database.");
+});
+
+module.exports = db;
